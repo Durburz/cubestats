7ceee94313199a30a75155ad070b9fbf8344c8a7
ufi <ufi@netzlink.com>
2014-07-23 11:14:38 +0200

    Added Events for statistics and split up DB save function

--- src/eu/valkyr/cubestats/cubestats.java
+++ src/eu/valkyr/cubestats/cubestats.java
@@ -9,15 +9,21 @@ import java.util.logging.Logger;
 import lib.PatPeter.SQLibrary.MySQL;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -28,8 +34,9 @@ public class cubestats extends JavaPlugin implements Listener {
 	private BukkitTask writer;
 	private ArrayList<Object[]> sessions = new ArrayList<Object[]>();
 	private ArrayList<Object[]> enchants = new ArrayList<Object[]>();
-	private ArrayList<Object[]> blocks = new ArrayList<Object[]>();
 	private ArrayList<Object[]> kills = new ArrayList<Object[]>();
+	private ArrayList<Object[]> crafts = new ArrayList<Object[]>();
+	private ArrayList<Object[]> smelts = new ArrayList<Object[]>();
 	
 	
 	public void onEnable() {
@@ -59,16 +66,23 @@ public class cubestats extends JavaPlugin implements Listener {
 					e.printStackTrace();
 				}
     		}
-			if(sql.isTable("blocks") == false){
+			if(sql.isTable("enchants") == false){
     			try {
-					sql.insert("CREATE TABLE blocks (UUID VARCHAR(36), count INT, block INT);");
+					sql.insert("CREATE TABLE enchants (UUID VARCHAR(36), count INT, item VARCHAR(20));");
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
     		}
-			if(sql.isTable("enchants") == false){
+			if(sql.isTable("crafts") == false){
     			try {
-					sql.insert("CREATE TABLE enchants (UUID VARCHAR(36), time INT, item INT);");
+					sql.insert("CREATE TABLE crafts (UUID VARCHAR(36), count INT, item VARCHAR(20));");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+    		}
+			if(sql.isTable("smelts") == false){
+    			try {
+					sql.insert("CREATE TABLE smelts (UUID VARCHAR(36), count INT, item VARCHAR(20));");
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -103,41 +117,22 @@ public class cubestats extends JavaPlugin implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void EnchantItemEvent(Player enchanter, InventoryView view, Block table, ItemStack item, int level, Map<Enchantment,Integer> enchants, int i) {
-		
+		addEnchant(enchanter.getUniqueId().toString(), item.getType().toString());
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void CraftItemEvent(Recipe recipe, InventoryView what, InventoryType.SlotType type, int slot, ClickType click, InventoryAction action) {
+		addCraft(what.getPlayer().getUniqueId().toString(),recipe.getResult().toString(), what.getItem(slot).getAmount());
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void FurnaceExtractEvent(Player player, Block block, Material itemType, int itemAmount, int exp)   {
+		addSmelt(player.getUniqueId().toString(),itemType.toString(), itemAmount);
 	}
 	
 	public void writeToDB() {
 		
-		for(int i=0; i < sessions.size(); i++) {
-			
-			if (sessions.get(i)[3] != null) {
-				try {
-					sql.insert("UPDATE session SET end='"+sessions.get(i)[2]+"' WHERE sessid='"+sessions.get(i)[3]+"';");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			else {
-				try {
-					sql.insert("INSERT INTO session (UUID,start,end) VALUES ('"+sessions.get(i)[0]+"','"+sessions.get(i)[1]+"','"+sessions.get(i)[2]+"');");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				ResultSet re;
-				try {
-					re = sql.query("SELECT sessid FROM session WHERE UUID='"+sessions.get(i)[0]+"' AND start='"+sessions.get(i)[1]+"';");
-					while(re.next()) {
-						sessions.get(i)[3] = re.getInt("sessid");
-					}
-					re.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		}
+		session2db();
 	}
 	
 	public void updateTimings() {
@@ -179,16 +174,105 @@ public class cubestats extends JavaPlugin implements Listener {
 		sessions.add(session);
 	}
 	
-	public void addEnchant(String id) {
-
-	}
-	
-	public void addBlock(String id) {
-
+	synchronized public void addEnchant(String id, String item) {
+		
+		Object[] enchant = new Object[3];
+		
+		enchant[0] = id;
+		enchant[1] = item;
+		enchants.add(enchant);
 	}
 	
 	public void addKill(String id) {
 
+	}
+	
+	synchronized public void addCraft(String id, String item, int count) {
+
+		Object[] craft = new Object[4];
+		
+		craft[0] = id;
+		craft[1] = item;
+		craft[2] = count;
+		crafts.add(craft);
+	}
+	
+	synchronized public void addSmelt(String id, String item, int count) {
+
+		Object[] smelt = new Object[4];
+		
+		smelt[0] = id;
+		smelt[1] = item;
+		smelt[2] = count;
+		smelts.add(smelt);
+	}
+	
+	public void session2db() {
+		for(int i=0; i < sessions.size(); i++) {
+			
+			if (sessions.get(i)[3] != null) {
+				try {
+					sql.insert("UPDATE session SET end='"+sessions.get(i)[2]+"' WHERE sessid='"+sessions.get(i)[3]+"';");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else {
+				try {
+					sql.insert("INSERT INTO session (UUID,start,end) VALUES ('"+sessions.get(i)[0]+"','"+sessions.get(i)[1]+"','"+sessions.get(i)[2]+"');");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					ResultSet re;
+					re = sql.query("SELECT sessid FROM session WHERE UUID='"+sessions.get(i)[0]+"' AND start='"+sessions.get(i)[1]+"';");
+					while(re.next()) {
+						sessions.get(i)[3] = re.getInt("sessid");
+					}
+					re.close();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void enchant2db() {
+		
+		int pre = 0;
+		
+		for(int i=0; i < enchants.size(); i++) {
+			try {
+				ResultSet re;
+				re = sql.query("SELECT count FROM enchants WHERE UUID='"+enchants.get(i)[0]+"' AND item='"+enchants.get(i)[1]+"';");
+				while(re.next()) {
+					pre = re.getInt("count");
+				}
+				re.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			if(pre != 0) {
+				try {
+					sql.insert("UPDATE enchants SET count='"+(pre+1)+"' WHERE UUID='"+enchants.get(i)[0]+"' AND item='"+enchants.get(i)[1]+"';");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else {
+				try {
+					sql.insert("INSERT INTO session (UUID,count,item) VALUES ('"+enchants.get(i)[0]+"','1','"+enchants.get(i)[1]+"');");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 }
